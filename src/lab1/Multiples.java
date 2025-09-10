05f83876425530804dc5b41dd102e6b74d0602c0
Jiafeng Qin <jennifer.qin@mail.utoronto.ca>
2025-09-09 21:51:51 -0400

    Merge branch 'main' of https://github.com/c0ffeec0des/FizzBuzz

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,10 +3,21 @@ package lab1;
 public class Multiples {
     public static void main(String[] args) {
         // Test example
-        int count = multiples(1000, 3, 5);
+        int count = multiples(200, 4, 7);
         System.out.println(count);
+
+        // Calling overloaded method
+        int count2 = multiples();
+        System.out.println(count2);
     }
 
+    // Overloaded method
+    public static int multiples() {
+        return multiples(1000, 3, 5);
+
+    }
+
+    // Original method with parameters
     public static int multiples(int n, int a, int b) {
         int numOfMultiples = 0;
         for (int i = 1; i < n; i++) {
